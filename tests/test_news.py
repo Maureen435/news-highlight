7dba6a464d8c0363115f14e8d21f724defa78d4d
Maureen435 <mchepkemoi435@gmail.com>
2020-07-03 13:22:31 +0300

    modified the test  case

--- tests/test_news.py
+++ tests/test_news.py
@@ -15,3 +15,18 @@ class SourceTest(unittest.TestCase):
         self.assertEquals(self.new_source.category,'general')
         self.assertEquals(self.new_source.country,'U.S.A')
         self.assertEquals(self.new_source.language,'en')
+
+class ArticlesTest(unittest.TestCase):
+    def setUp(self):
+        self.new article = Articles('CNN','Memzo','The software World','A look at various software developers who have made the current technological world important and fruitful','mem.com','personal','Kenya','en')
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_article,Articles))
+    def test_to_check_instance_variables(self):
+        self.assertEquals(self.new_source.id,'CNN')
+        self.assertEquals(self.new_source.name,'Memzo')
+        self.assertEquals(self.new_source.title,'The software World')
+        self.assertEquals(self.new_source.description,'A look at various software developers who have made the current technological world important and fruitful')
+        self.assertEquals(self.new_source.url,'mem.com')
+        self.assertEquals(self.new_source.category,'personal')
+        self.assertEquals(self.new_source.country,'Kenya')
+        self.assertEquals(self.new_source.language,'en')
