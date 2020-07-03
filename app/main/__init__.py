48c2388d95eb2be4726b77fe238ddd405eddcad6
Maureen435 <mchepkemoi435@gmail.com>
2020-07-03 14:58:23 +0300

    initial commit

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -1,20 +1,3 @@
-from flask import Flask 
-from flask_bootstrap import Bootstrap 
-from config import config_options
-
-bootstrap = Bootstrap()
-
-def create_app(config_name):
-    app = Flask(__name__)
-
-    #create the app configurations
-    app.config.from_object(config_options[config_name])
-
-    #initializing flask extensions
-    bootstrap.init_app(app)
-
-    #registering the blueprint
-    from.requests import configure_requests
-    configure_request(app)
-
-    return app
+from flask import Blueprint
+main = Blueprint('main',__name__)
+from . import views,errors
